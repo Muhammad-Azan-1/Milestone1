3b17ef8844a5217d30e8bcc44674e1fa632e0c53
Muhammad Azan <155957067+Muhammad-Azan-1@users.noreply.github.com>
2024-09-18 12:35:45 +0500

    first commint

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var btn = document.querySelector('btn');
+var btn = document.querySelector('.btn');
+console.log(btn);
 btn.addEventListener('click', function (e) {
-    window.open('../milestone3/index.html', '_blank');
+    window.open('https://milestone2-2cy6njvo1-muhammad-azans-projects-b0c48043.vercel.app/');
 });
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-let btn = document.querySelector('btn') as HTMLButtonElement;
-
+let btn = document.querySelector('.btn') as HTMLButtonElement;
+console.log(btn);
 
 btn.addEventListener('click', (e) => {
 
-    window.open('../milestone3/index.html', '_blank')
+    window.open('https://milestone2-2cy6njvo1-muhammad-azans-projects-b0c48043.vercel.app/')
 
 })  
