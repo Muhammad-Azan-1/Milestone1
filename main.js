f6f9dcfd8c850dcff04a4d81c3e9314c831c63b5
Muhammad Azan <155957067+Muhammad-Azan-1@users.noreply.github.com>
2024-09-18 12:12:29 +0500

    edit login in ts so that onclick it navigate to the milestone3

---
+++ main.js
@@ -0,0 +1,4 @@
+var btn = document.querySelector('btn');
+btn.addEventListener('click', function (e) {
+    window.open('../milestone3/index.html', '_blank');
+});
---
+++ main.ts
@@ -0,0 +1,8 @@
+let btn = document.querySelector('btn') as HTMLButtonElement;
+
+
+btn.addEventListener('click', (e) => {
+
+    window.open('../milestone3/index.html', '_blank')
+
+})  
